acc8502bced45cea2e759cce0b2a25c9ac6a6472
sam caldwell <mail@samcaldwell.net>
2023-06-02 01:32:57 -0500

    add docstring for PostitionalArguments.go

--- argparse/PositionalArguments.go
+++ argparse/PositionalArguments.go
@@ -2,6 +2,7 @@ package argparse
 
 import "github.com/sam-caldwell/go-argparse/v2/types"
 
+// PositionalArguments - This struct represents a positional argument struct
 type PositionalArguments struct {
 	argName    types.ArgString
 	argType    types.ArgTypes
