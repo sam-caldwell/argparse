1e78d4150e1fc88a82c706e42e0774f33f7a1af4
sam caldwell <mail@samcaldwell.net>
2023-06-03 18:58:48 -0500

    update example

--- example/main.go
+++ example/main.go
@@ -9,75 +9,17 @@ import (
 // main - Demonstration program
 
 func main() {
-	var err error
-	var args argparse.Arguments
 	log.Println("starting example...")
-
-	log.Println("Initialize Arguments")
-	args.Init("This is an example program implementing argparse")
-	args.EnableDebugging()
-
-	log.Println("Add positional argument pos1")
-	err = args.AddArgument(
-		"pos1",
-		types.String,
-		false,
-		"pos1value",
-		"This is pos1 help")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Add positional argument pos2")
-	err = args.AddArgument(
-		"pos2",
-		types.Integer,
-		false,
-		1337,
-		"This is pos2 help")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Add optional argument -a")
-	err = args.AddArgument(
-		"-a",
-		types.Integer,
-		true,
-		-1,
-		"This is -a <integer> argument help")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Add optional argument --flag")
-	err = args.AddArgument(
-		"--flag",
-		types.Flag,
-		true,
-		false,
-		"This is --flag argument help")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Parse arguments against os.Args")
-	err = args.ParseArgs()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("-------Dump() parsed args--------")
-	for i, v := range args.Dump() {
-		log.Println(i, v)
-	}
-	log.Println("---------------------------------")
-
-	if value, err := args.Get("pos1"); err != nil {
-		log.Fatalf("expected pos1 arg not found: %v", err)
-	} else {
-		log.Printf("expected pos1 arg value: %v", value)
-	}
-
-	log.Println("done...")
+	var args argparse.Arguments
+	args.
+		ProgramName().
+		Copyright(2023, "Sam Caldwell", "mail@samcaldwell.net>").
+		Preamble("This is a description of our Arguments Object.").
+		Postscript("This is the postfix string after help is dumped.").
+		ShowErrors().
+		Add("all", "-a", "--all", types.Flag, true, false, "All flag").
+		Add("number", "-n", "--number", types.Integer, true, 1337, "set number").
+		Add("bool", "-b", "--bool", types.Boolean, true, false, "set boolean").
+		ExitOnError().
+		Parse()
 }
