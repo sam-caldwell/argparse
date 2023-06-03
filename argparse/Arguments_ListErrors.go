1a8f881e532fcb73af5ce67739fc466d5bf042bc
sam caldwell <mail@samcaldwell.net>
2023-06-03 18:57:40 -0500

    Add Arguments.ListErrors()

--- argparse/Arguments_ListErrors.go
+++ argparse/Arguments_ListErrors.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // ListErrors - List any errors and return the string list
 func (arg *Arguments) ListErrors() (result []string) {
-	if arg.hasErrors() {
+	if arg.HasErrors() {
 		fmt.Println(errParsingCliArgs)
 		for _, err := range arg.err {
 			if err != nil {
