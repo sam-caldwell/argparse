e8566dc630a623c598b9986c2afa1e8f352eb06a
sam caldwell <mail@samcaldwell.net>
2023-06-03 18:55:56 -0500

    add Arguments .HasErrors() and .ShowErrors()

--- argparse/Arguments_HasErrors.go
+++ argparse/Arguments_HasErrors.go
@@ -1,5 +1,6 @@
 package argparse
 
+// HasErrors - return true if there are any errors in the Arguments struct
 func (arg *Arguments) HasErrors() bool {
 	return len((*arg).err) > 0
 }
--- argparse/Arguments_ShowErrors.go
+++ argparse/Arguments_ShowErrors.go
@@ -2,8 +2,9 @@ package argparse
 
 import "fmt"
 
+// ShowErrors - print the list of errors to stdout
 func (arg *Arguments) ShowErrors() *Arguments {
-	if arg.hasErrors() {
+	if arg.HasErrors() {
 		fmt.Println("Error parsing command-line arguments")
 		for _, err := range arg.err {
 			if err != nil {
