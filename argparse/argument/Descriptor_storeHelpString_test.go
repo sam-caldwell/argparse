b0bd271983b8b59d843f0717ce725747c1925cca
sam caldwell <mail@samcaldwell.net>
2023-06-04 00:37:55 -0500

    Add more tests

--- argparse/argument/Descriptor_storeHelpString_test.go
+++ argparse/argument/Descriptor_storeHelpString_test.go
@@ -1,1 +1,17 @@
 package argument
+
+import "testing"
+
+func TestDescriptor_storeHelpString(t *testing.T) {
+	var descriptor Descriptor
+
+	actual := "test help"
+	expected := "test help"
+
+	if err := descriptor.storeHelpString(&actual); err != nil {
+		t.Fatal(err)
+	}
+	if result := descriptor.GetHelp(); result != expected {
+		t.Fatal("actual/expected mismatch")
+	}
+}
