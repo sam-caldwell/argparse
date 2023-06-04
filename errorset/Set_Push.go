38664ef14caeff8a3cfe004deb50f4561ade9a09
sam caldwell <mail@samcaldwell.net>
2023-06-03 20:09:57 -0500

    push non-nil Set.Push()

--- errorset/Set_Push.go
+++ errorset/Set_Push.go
@@ -1,8 +1,8 @@
 package errorset
 
-import "fmt"
-
-// Push - Add an error to Set array
-func (err *Set) Push(m string) {
-	(*err).e = append((*err).e, fmt.Errorf(m))
+// Push - Add an error to Set array assuming it's not nil
+func (err *Set) Push(e error) {
+	if e != nil {
+		(*err).e = append((*err).e, e)
+	}
 }
