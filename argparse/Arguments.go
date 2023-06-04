6ff0a77362cd34581e0243e5f8e0829e6cffaaa9
sam caldwell <mail@samcaldwell.net>
2023-06-03 20:10:57 -0500

    change []error to errorset.Set in Arguments

--- argparse/Arguments.go
+++ argparse/Arguments.go
@@ -1,11 +1,13 @@
 package argparse
 
+import "github.com/sam-caldwell/go-argparse/v2/errorset"
+
 // Arguments - Top-Level ArgParse struct
 type Arguments struct {
 	set            []Argument
 	programName    string
 	preambleText   []string
 	postscriptText []string
-	err            []error
+	err            errorset.Set
 	value          map[string]interface{}
 }
