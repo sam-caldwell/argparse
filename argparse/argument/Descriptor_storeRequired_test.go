cc12ca25e1132d8801f2d2bcb7d8c1363295d6d9
sam caldwell <mail@samcaldwell.net>
2023-06-04 01:05:02 -0500

    Fixing storeRequired() and add working tests

--- argparse/argument/Descriptor_storeRequired_test.go
+++ argparse/argument/Descriptor_storeRequired_test.go
@@ -9,16 +9,15 @@ func TestDescriptor_storeRequired_int64(t *testing.T) {
 	var descriptor Descriptor
 
 	argType := types.Integer
-	argRequired := true
 	argDefault := int64(1000)
 
-	if err := descriptor.storeRequired(types.Integer, argRequired, argDefault); err != nil {
+	if err := descriptor.storeRequired(types.Integer, true, argDefault); err != nil {
 		t.Fatal(err)
 	}
 	if descriptor.typ != argType {
 		t.Fatalf("type mismatch %v %v", descriptor.typ, argType)
 	}
-	if descriptor.required != argRequired {
+	if descriptor.required != true {
 		t.Fatal("required mismatch")
 	}
 	if descriptor.defaultValue.(int64) != argDefault {
