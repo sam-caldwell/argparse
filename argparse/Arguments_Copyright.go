c9f9285cd8d57415bb5dcbb7a8b12ed01e77f5df
sam caldwell <mail@samcaldwell.net>
2023-06-03 20:21:44 -0500

    add Arguments.Copyright()

--- argparse/Arguments_Copyright.go
+++ argparse/Arguments_Copyright.go
@@ -2,17 +2,18 @@ package argparse
 
 import (
 	"fmt"
-	"regexp"
+	"github.com/sam-caldwell/go-argparse/v2/valid"
 	"strings"
 )
 
 // Copyright - Append the Copyright string to the preamble.
 func (arg *Arguments) Copyright(year int, author string, email string) *Arguments {
-	const validYearRegex = `^[0-9]{4}$`
 
-	yearString := strings.TrimSpace(fmt.Sprintf("%d", year))
-	if (year < 2023) || !regexp.MustCompile(validYearRegex).MatchString(yearString) {
-		arg.err = fmt.Errorf("invalid year")
+	var err error
+
+	yearString, err := valid.IsValidYear(year)
+	if err != nil {
+		arg.err.Push(err)
 	}
 
 	author = strings.TrimSpace(author)
