e518e90cb835c2e42b07080ca50a15905ad91dfe
sam caldwell <mail@samcaldwell.net>
2023-06-04 01:04:04 -0500

    Fixing storeRequired() and add working tests

--- argparse/argument/Descriptor_storeRequired.go
+++ argparse/argument/Descriptor_storeRequired.go
@@ -1,16 +1,13 @@
 package argument
 
 import (
-	"fmt"
 	"github.com/sam-caldwell/argparse/v2/types"
 )
 
 // storeRequired - store the required flag and type-check the default value
 func (arg *Descriptor) storeRequired(t types.ArgTypes, required bool, argDefault any) (err error) {
 	arg.defaultValue = argDefault
-	if required && (argDefault == nil) {
-		return fmt.Errorf(errMissingDefault, t.String())
-	}
 	arg.required = required
+	arg.typ = t
 	return t.Typecheck(argDefault)
 }
--- argparse/argument/Descriptor_storeRequired_test.go
+++ argparse/argument/Descriptor_storeRequired_test.go
@@ -1,1 +1,27 @@
 package argument
+
+import (
+	"github.com/sam-caldwell/argparse/v2/types"
+	"testing"
+)
+
+func TestDescriptor_storeRequired_int64(t *testing.T) {
+	var descriptor Descriptor
+
+	argType := types.Integer
+	argRequired := true
+	argDefault := int64(1000)
+
+	if err := descriptor.storeRequired(types.Integer, argRequired, argDefault); err != nil {
+		t.Fatal(err)
+	}
+	if descriptor.typ != argType {
+		t.Fatalf("type mismatch %v %v", descriptor.typ, argType)
+	}
+	if descriptor.required != argRequired {
+		t.Fatal("required mismatch")
+	}
+	if descriptor.defaultValue.(int64) != argDefault {
+		t.Fatal("defaultValue mismatch")
+	}
+}
