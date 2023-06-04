83f534187afed2921b0f79c5ce9e24f2afaa2a8d
sam caldwell <mail@samcaldwell.net>
2023-06-04 00:43:03 -0500

    Descriptor.storeLong() test added

--- argparse/argument/Descriptor_storeLong_test.go
+++ argparse/argument/Descriptor_storeLong_test.go
@@ -1,1 +1,17 @@
 package argument
+
+import "testing"
+
+func TestDescriptor_storeLong(t *testing.T) {
+	var descriptor Descriptor
+
+	actual := "--Test"
+	expected := "test"
+
+	if err := descriptor.storeLong(&actual); err != nil {
+		t.Fatal(err)
+	}
+	if result := descriptor.GetLong(); result != expected {
+		t.Fatalf("actual/expected mismatch %s %s", result, expected)
+	}
+}
