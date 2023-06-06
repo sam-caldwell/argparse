7a5f96e49bb79f30e8bb1b631ffdfb0445148627
sam caldwell <mail@samcaldwell.net>
2023-06-05 22:52:36 -0500

    Optimize Arguments.Preamble() and test

--- argparse/Arguments.Preamble.go
+++ argparse/Arguments.Preamble.go
@@ -2,8 +2,6 @@ package argparse
 
 // Preamble - Add another line to the prefix set
 func (arg *Arguments) Preamble(line string) *Arguments {
-	if err := arg.preambleText.Add(line); err != nil {
-		_ = arg.err.Push(err)
-	}
+	_ = arg.err.Push(arg.preambleText.Add(line))
 	return arg
 }
