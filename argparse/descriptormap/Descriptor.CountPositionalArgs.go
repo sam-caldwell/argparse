b904307ccb66c59f66ef8d521937849d2a5013ad
sam caldwell <mail@samcaldwell.net>
2023-06-07 18:17:35 -0500

    add missing docstring

--- argparse/descriptormap/Descriptor.CountPositionalArgs.go
+++ argparse/descriptormap/Descriptor.CountPositionalArgs.go
@@ -1,5 +1,6 @@
 package descriptormap
 
+// CountPositionalArgs - return a count of positional arguments
 func (m *Map) CountPositionalArgs() (count int) {
 	for _, argument := range m.data {
 		if (argument.GetShort() == "") || (argument.GetLong() == "") {
