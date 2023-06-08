a09e27256405c64006ae989b7dac92bc0d133541
sam caldwell <mail@samcaldwell.net>
2023-06-07 23:25:38 -0500

    Code clean up (parse is wip)

--- argparse/Arguments.Parse.go
+++ argparse/Arguments.Parse.go
@@ -17,7 +17,7 @@ func (arg *Arguments) Parse() *Arguments {
 	var thisName *string
 	var thisDescriptor *descriptor.Descriptor = nil
 
-	for _, rawToken := range os.Args[1:] {
+	for position, rawToken := range os.Args[1:] {
 
 		// clean the token any intentional whitespace should be wrapped in quotes.
 		token := strings.TrimSpace(rawToken)
@@ -66,26 +66,41 @@ func (arg *Arguments) Parse() *Arguments {
 			continue //Just go get the next one
 		}
 
-		//Deal with the positional arguments
-		if err := valid.IsNameArg(&token); err == nil {
-			if optionalArgEncountered {
-				_ = arg.err.Push(fmt.Errorf(errPositionalArgumentFollowingOptional))
-			}
-			thisName, thisDescriptor = arg.descriptors.GetByName(&token)
-			if thisName == nil {
-				_ = arg.err.Push(fmt.Errorf(errFoundUndefinedArgument, token))
-				return arg
-			}
-			//Flags have no values.  We can just store them.
-			if typ := thisDescriptor.GetType(); typ == types.Flag {
-				var value any = true
-				_ = arg.err.Push(arg.results.Add(thisName, types.Flag, &value))
-			}
-			valueExpected = true //we need the next item to be a value
-			continue             //Just go get the next one
+		//Deal with the positional arguments (token is a value now)
+		if optionalArgEncountered {
+			//If we have already encountered a short -a or long --all optional argument, bail.
+			_ = arg.err.Push(fmt.Errorf(errPositionalArgumentFollowingOptional))
+			return arg
 		}
-		_ = arg.err.Push(fmt.Errorf(errParsingCliArgs, token))
-		return arg
-	}
+
+		//Otherwise look up our expected argument by position.
+		thisName, thisDescriptor = arg.descriptors.GetByPosition(position)
+
+		//But if the result (thisName) is nil, we weren't expecting an arg at this position.  bail.
+		if thisName == nil {
+			// if position not found in our descriptor map then we have a syntax error.
+			_ = arg.err.Push(fmt.Errorf(errFoundUndefinedArgument, token))
+			return arg
+		}
+
+		//However, if all this works out, token must be a positional value.  Type-check it. Bail on fail.
+		typ := thisDescriptor.GetType()
+		if err := typ.Typecheck(token); err != nil {
+			_ = arg.err.Push(err)
+			return arg
+		}
+
+		// Store this value
+		var value any = token
+		if err := arg.results.Add(thisName, typ, &value); err != nil {
+			panic(err)
+		}
+
+		// Set our state to expect a new token
+		valueExpected = false
+
+		continue
+	} /*end of for*/
+	_ = arg.err.Push(fmt.Errorf(errParsingCliArgs, *thisName))
 	return arg
 }
--- argparse/Arguments.Parse_test.go
+++ argparse/Arguments.Parse_test.go
@@ -1,12 +1,13 @@
 package argparse
 
 import (
+	"github.com/sam-caldwell/argparse/v2/argparse/types"
 	"os"
 	"testing"
 )
 
 func TestArguments_Parse(t *testing.T) {
-	//var args Arguments
+	var args Arguments
 
 	//failIf := func(condition bool, msg string, params ...any) {
 	//	if condition {
@@ -24,20 +25,20 @@ func TestArguments_Parse(t *testing.T) {
 		"-f", "1.0", //short arg (float)
 		"--long", "yes", //long arg (string)
 	}
-	//
-	//args.
-	//	ProgramName().
-	//	Copyright(2023, "Sam Caldwell", "mail@samcaldwell.net>").
-	//	Preamble("This is a description of our Arguments Object.").
-	//	Postscript("This is the postfix string after help is dumped.").
-	//	Add("a0pos", "", "", types.String, true, "not set", "pos arg 1").
-	//	Add("a1pos", "", "", types.String, true, "not set", "pos arg 2").
-	//	Add("all", "-a", "--all", types.Flag, true, false, "All flag").
-	//	Add("number", "-n", "--number", types.Integer, true, 0, "set num").
-	//	Add("float", "-f", "--float", types.Boolean, true, 0.0, "set float").
-	//	Add("long", "-l", "--long", types.String, true, "no", "set bool").
-	//	ExitOnError().
-	//	Parse()
+
+	args.
+		ProgramName().
+		Copyright(2023, "Sam Caldwell", "mail@samcaldwell.net>").
+		Preamble("This is a description of our Arguments Object.").
+		Postscript("This is the postfix string after help is dumped.").
+		Add("a0pos", "", "", types.String, true, "not set", "pos arg 1").
+		Add("a1pos", "", "", types.String, true, "not set", "pos arg 2").
+		Add("all", "-a", "--all", types.Flag, true, false, "All flag").
+		Add("number", "-n", "--number", types.Integer, true, 0, "set num").
+		Add("float", "-f", "--float", types.Boolean, true, false, "set float").
+		Add("long", "-l", "--long", types.String, true, "no", "set bool").
+		ExitOnError().
+		Parse()
 	//
 	//if args.ErrorCount() != 0 {
 	//	for _, e := range args.Errors() {
