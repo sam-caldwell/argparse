d78f605a9467bf60fb8345f876396c6d43ceb60f
sam caldwell <mail@samcaldwell.net>
2023-06-07 20:23:48 -0500

    code cleanup (strings to constants) and remove unused parameter for function

--- argparse/descriptormap/Descriptor.Map.FindDuplicates.go
+++ argparse/descriptormap/Descriptor.Map.FindDuplicates.go
@@ -2,24 +2,23 @@ package descriptormap
 
 import (
 	"fmt"
-	"github.com/sam-caldwell/argparse/v2/argparse/descriptormap/descriptor"
 	"github.com/sam-caldwell/simpleset/v2"
 )
 
-func (m *Map) FindDuplicates(d descriptor.Descriptor) error {
+func (m *Map) FindDuplicates() error {
 	var argSet simpleset.Set
 	var shortSet simpleset.Set
 	var longSet simpleset.Set
 
 	for name, argument := range m.data {
 		if err := argSet.Add(name); err != nil {
-			return fmt.Errorf("argument cannot be redefined %s", name)
+			return fmt.Errorf(errArgumentCannotBeRedefined, name)
 		}
 		if err := shortSet.Add(argument.GetShort()); err != nil {
-			return fmt.Errorf("short argument cannot be redefined %s", name)
+			return fmt.Errorf(errArgumentCannotBeRedefined, name)
 		}
 		if err := longSet.Add(argument.GetLong()); err != nil {
-			return fmt.Errorf("long argument cannot be redefined %s", name)
+			return fmt.Errorf(errArgumentCannotBeRedefined, name)
 		}
 	}
 	return nil
